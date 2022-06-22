62c96dd8c30781c41545f7304b8af65956b4866f
Nitin Jaiswal <bhagatnitin254@gmail.com>
2022-06-23 00:57:23 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function ClockComponent(props) {
+  const [updatedTime, setUpdatedTime] = useState(new Date(props.time));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setUpdatedTime(new Date(updatedTime.getTime() + 1000));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [updatedTime]);
+
+  return (
+    <h2>
+      {updatedTime.getHours()}:{updatedTime.getMinutes()}:
+      {updatedTime.getSeconds()}
+      <br />
+      <button
+        onClick={() => {
+          setUpdatedTime(new Date(updatedTime.getTime() - 3600000));
+        }}
+      >
+        Reverse Time By 1 Hour
+      </button>
+    </h2>
+  );
+}
 
 function App() {
+  const [clockState, setClockState] = useState(Date());
+
+  useEffect(() => {
+    fetch("http://worldtimeapi.org/api/timezone/Europe/Berlin")
+      .then((result) => result.json())
+      .then((result) => {
+        let time = new Date(result.datetime);
+
+        setClockState(time.getTime());
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Time</h1>
+      <ClockComponent time={clockState} />
     </div>
   );
 }
